5742b9e65aab57e80b4b34f4bf51fc554c6356ef
jian <jian.kootker@gmail.com>
2022-04-20 10:59:50 +0200

    example component

---
+++ src/components/movie-preview/MoviePreview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './moviePreview.module.css';
+
+function moviePreview() {
+  return (
+    <div>
+      <h1 className={styles.header}>moviePreview</h1>
+    </div>
+  );
+}
+
+export default moviePreview;
